3e38fa7655a95ec752a92529e4e9d0080e82a275
Student <student@itstep.ge>
2023-01-14 18:52:43 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// function getWage(x,y,z){
+//     return x + (y * z)
+// }
+
+// let employ = {
+//     salary:5000,
+//     overtime:10,
+//     rate:20,
+// }
+
+
+
+// let test = getWage(employ.salary,employ.overtime,employ.rate)
+
+// console.log(test)
+
+// class Car{
+//     constructor(name){
+//         this.name = name
+//     }
+
+//     static printHello(){
+//         // return `console.log(my name is ${this.name})`
+//         console.log(`console.log(my name is ${this.name})`)
+//     }
+// }
+// Car.printHello();
+
+// let car = new Car('Julio')
+
+// console.log(car.printHello())
+
+
+// function getHumanInfo(name,lastName,age){
+//     return{
+//         name:name,
+//         lastName:lastName,
+//         age:age
+//     }
+// }
+// let human = getHumanInfo("Andria","Konstantinidi",13)
+
+// console.log(human)
